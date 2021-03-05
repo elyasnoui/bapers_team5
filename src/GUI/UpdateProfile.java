a986456c09e250799afc86f3fdb1f71979754d3f
Elyas Noui <elyas.noui@city.ac.uk>
2021-03-05 16:32:36 +0000

    Initial commit

--- src/GUI/UpdateProfile.java
+++ src/GUI/UpdateProfile.java
@@ -4,7 +4,16 @@ import javax.swing.*;
 
 public class UpdateProfile {
     private JPanel mainPanel;
+    private JPanel contentPanel;
+    private JPanel sidePanel;
+    private JLabel bannerLabel;
+    private JTable table1;
+    private ImageIcon bannerIcon;
 
+    public UpdateProfile() {
+        bannerIcon = new ImageIcon("data/banners/test2.png");
+        bannerLabel.setIcon(bannerIcon);
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
