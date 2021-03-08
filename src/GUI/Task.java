cea5751a0e3a389dbb116fcd799d49812ad561d3
Asad Zafar <Asad.Zafar@city.ac.uk>
2021-03-08 15:26:07 +0000

    added Task page

--- src/GUI/Task.java
+++ src/GUI/Task.java
@@ -1,8 +1,78 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Task {
+
+
+    private JButton CreateButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JTable taskTable;
+    private JPanel mainPanel;
+    private JButton createButton;
+    private JLabel bannerLabel;
+    private JPanel contentPanel;
+    private JPanel buttonPanel;
+    private JPanel sidePanel;
+    private JPanel navigationPanel;
+    private JButton customerButton;
+    private JButton paymentButton;
+    private JButton staffButton;
+    private JButton taskButton;
+    private JButton jobButton;
+    private JButton reportButton;
+    private ImageIcon bannerIcon;
+
+
+    private String[] ColumnNames = {
+            "ID",
+            "Description",
+            "Department",
+            "Duration (MIN)",
+            "PRICE (£)",
+    };
+
+    private Object[][] data = {
+            {"01", "Use of large Copy Camera", "Copy Room", "120", "19.00"}
+    };
+
+
+    public Task() {
+        bannerIcon = new ImageIcon("data/banners/staff.png");
+        bannerLabel.setIcon(bannerIcon);
+
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Create Staff");
+            }
+        });
+
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Delete Staff");
+            }
+        });
+
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Edit Staff");
+            }
+        });
+
+
+
+        createTable();
+
+    }
+
     public JPanel getMainPanel() {
         return mainPanel;
     }
@@ -10,6 +80,11 @@ public class Task {
     public void setMainPanel(JPanel mainPanel) {
         this.mainPanel = mainPanel;
     }
+    private void createTable(){
+        taskTable.setModel(new DefaultTableModel(data,ColumnNames));
+        TableColumnModel columns = taskTable.getColumnModel();
+        columns.getColumn(4).setMinWidth(100);
+    }
 
-    private JPanel mainPanel;
+
 }
